771a7f3e5fb79a2bc047f9c4b906d7a505376a4d
Ivy-ding99 <130305532+Ivy-ding99@users.noreply.github.com>
2023-08-10 22:36:14 -0400

    Update cyclic url

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ I declare that this assignment is my own work in accordance with Seneca  Academi
 Name: _Hui Ding
 Student ID: 119835221
 Date: 2023-08-09
-Cyclic Web App URL: https://ruby-better-hummingbird.cyclic.app/About
+Cyclic Web App URL: https://long-teal-pangolin-toga.cyclic.app/
 GitHub Repository URL: https://github.com/Ivy-ding99/web322-app.git
 
 
